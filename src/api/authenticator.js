f8cf9bd34b4994e1e8ad34efc5b4b4dde4ace483
Pergamene <smartz373@gmail.com>
2020-04-29 08:39:25 -0600

    changed auth error response

--- src/api/authenticator.js
+++ src/api/authenticator.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
   if (token) {
     jwt.verify(token, secret, (error, decodedToken) => {
       if (error) {
-        res.status(401).json({ message: "Invalid credentials." });
+        res.status(401).json({ message: error.message });
       } else {
         next();
       }
